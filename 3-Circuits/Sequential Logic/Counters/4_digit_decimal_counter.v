b2a747980f63b95aa3bd1a971a2d0e59089c4b3c
aditya-rv-vlsi <162978271+aditya-rv-vlsi@users.noreply.github.com>
2024-12-20 15:39:45 +0530

    Update 4_digit_decimal_counter.v

--- 3-Circuits/Sequential Logic/Counters/4_digit_decimal_counter.v
+++ 3-Circuits/Sequential Logic/Counters/4_digit_decimal_counter.v
@@ -1,3 +1,38 @@
+module top_module (
+    input clk,
+    input reset,   // Synchronous active-high reset
+    output [3:1] ena,
+    output [15:0] q);
+
+    decade_counter DC_U(.clk(clk), .enable(1), .reset(reset), .q(q[3:0]));
+    decade_counter DC_T(.clk(clk), .enable(ena[1]), .reset(reset), .q(q[7:4]));
+    decade_counter DC_H(.clk(clk), .enable(ena[2]), .reset(reset), .q(q[11:8]));
+    decade_counter DC_TH(.clk(clk),.enable(ena[3]), .reset(reset), .q(q[15:12]));
+    
+    assign ena[1] = (q[3:0]==4'd9);
+    assign ena[2] = (q[3:0]==4'd9)&&(q[7:4]==4'd9);
+    assign ena[3] = (q[3:0]==4'd9)&&(q[7:4]==4'd9)&&(q[11:8]==4'd9);   
+endmodule
+
+module decade_counter (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    input enable,
+    output [3:0] q);
+
+    always@(posedge clk)
+        if(reset)
+            q<=0;
+    	else begin
+            if(!enable)
+                q<=q;
+            else
+            	q<=(q==4'd9)?0:q+1;
+        end
+endmodule
+
+
+
 /*The Code is not fully correct in HDLBits*/
 
 module top_module (
