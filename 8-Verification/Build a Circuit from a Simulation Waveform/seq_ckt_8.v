9fbdd24a71f8fa6481c893efb3eccbeb45cf2447
aditya-rv-vlsi <162978271+aditya-rv-vlsi@users.noreply.github.com>
2024-12-20 16:22:38 +0530

    Create seq_ckt_8.v

---
+++ 8-Verification/Build a Circuit from a Simulation Waveform/seq_ckt_8.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clock,
+    input a,
+    output p,
+    output q );
+
+    always@(*)
+        if(clock)
+            p=a;
+    always@(negedge clock)
+        q<=p;
+endmodule
